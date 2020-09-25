e8d31d95c989470f9b87128a6338562382d1095b
Anna Pawl <annampawl@gmail.com>
2020-09-25 12:47:53 -0400

    load trivia data, install papaparse

---
+++ src/categories.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import * as d3 from 'd3'
+
+class Categories extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <h1>Categories</h1>
+      </div>
+    )
+  }
+}
+
+export default Categories
--- src/trivia.js
+++ src/trivia.js
@@ -1,11 +1,39 @@
 // Load Trivia Data
-d3.csv("trivia.csv").then(function(data) {
-  // data.forEach(function(d) {});
-  console.log(data);
-  handleTriviaData(data);
-});
+// async will return a promise
+async function loadTriviaData() {
+  const res = await fetch('data/test.csv')
+  const csv = await res.text()
+  // console.log(csv)
+
+  const arr = csv.split('\n')
+  const keys = arr[0].split('\t')
+  // console.log(keys)
+
+  const trivArr = []
+  for(let i=1; i < arr.length; i+=1){
+    const row = arr[i].split('\t')
+    const trivObj = {}
+    for(let j=0; j < row.length; j +=1){
+      trivObj[keys[j]] = row[j]
+     
+    }
+    trivArr.push(trivObj)
+  }
+  console.log(trivArr)
+
+
+  // const p = fetch('data/trivia.csv')
+  // const p2 = p.then((res)=> {
+  //   return res.text()
+  // })
+  // p2.then((csv)=> {
+  //   console.log(csv)
+  // })
+}
+
+loadTriviaData()
 
 // Handle Trivia Data
-function handleTriviaData(csv) {
-  
-}
+
+
+export default loadTriviaData
