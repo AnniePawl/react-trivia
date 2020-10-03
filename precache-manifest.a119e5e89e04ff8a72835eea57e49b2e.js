554d4324636fedd1db29d22f3d4dffa7a05e48e6
Anna Pawl <annampawl@gmail.com>
2020-10-03 12:13:01 -0400

    Updates

---
+++ precache-manifest.a119e5e89e04ff8a72835eea57e49b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7aa5210cf5621be1a624ad4d51f05ac",
+    "url": "/react-trivia/index.html"
+  },
+  {
+    "revision": "5e840f20535dd1392475",
+    "url": "/react-trivia/static/css/main.cb74d376.chunk.css"
+  },
+  {
+    "revision": "30e38e271a9c13e363b8",
+    "url": "/react-trivia/static/js/2.cf0051f7.chunk.js"
+  },
+  {
+    "revision": "cbf7ab8ad9b210a8e3f9021a24e25fd5",
+    "url": "/react-trivia/static/js/2.cf0051f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e840f20535dd1392475",
+    "url": "/react-trivia/static/js/main.2d26fd04.chunk.js"
+  },
+  {
+    "revision": "6951e0fcab12b25d047b",
+    "url": "/react-trivia/static/js/runtime-main.2d137bbc.js"
+  }
+]);
